600bf85e1b0d559e1b5ccc7be9ab05c593a6227b
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-26 10:23:37 -0500

    table revisions

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,16 +1,15 @@
-
-
 USE codeup_test_db;
+
 DROP TABLE IF EXISTS albums;
 
 -- INT, FLOAT, DECIMAL
 -- CHAR, VARCHAR, TEXT
 CREATE TABLE albums (
-    id INT,
-    artist VARCHAR,
-    name VARCHAR(100),
-    release_date INT,
-    sales FLOAT,
-    genre VARCHAR,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date INT NOT NULL,
+    sales DECIMAL(6, 3) NOT NULL,
+    genre VARCHAR(100),
     PRIMARY KEY (id)
 );
