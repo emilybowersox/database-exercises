f3fc5ea6cb8cc587bcddde8e48d63ac5a1a413ae
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-27 14:41:19 -0500

    chaining clauses

--- where_exercises.sql
+++ where_exercises.sql
@@ -27,3 +27,39 @@ where birth_date like '%12-25';
 select *
 from employees
 where last_name like '%q%';
+
+--
+
+# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+select *
+from employees
+where gender = 'M'
+and (first_name = 'Irena'
+or first_name = 'Vidya'
+or first_name = 'Maya');
+
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+select *
+from employees
+where last_name like 'E%'
+or last_name like '%e';
+
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+select *
+from employees
+where last_name like 'E%'
+   and last_name like '%e';
+
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
+select *
+from employees
+where hire_date like '199%'
+and birth_date like '%12-25';
+
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+-- ??
+select *
+from employees
+where last_name like '%q%'
+and last_name like '%qu%';
