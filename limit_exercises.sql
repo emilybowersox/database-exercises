9a727d0a24dd47444b046eab64e1a262f6ad7ce5
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-28 10:04:19 -0500

    limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,3 +7,10 @@ from employees
 order by last_name desc
 limit 10;
 
+select first_name, last_name
+from employees
+where hire_date like '199%'
+  and birth_date like '%12-25'
+order by birth_date, hire_date DESC
+limit 5 offset 45;
+
