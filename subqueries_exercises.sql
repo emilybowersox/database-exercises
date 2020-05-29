aa74412d166988ea216880549b0774bd3486a897
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-29 14:50:19 -0500

    exercise 1

---
+++ subqueries_exercises.sql
@@ -0,0 +1,31 @@
+use employees;
+
+
+# Find all the employees with the same hire date as employee 101010
+# using a subquery. (69 Rows)
+select concat(first_name, ' ', last_name) as full_name
+from employees
+where hire_date in (
+    select hire_date
+    from employees
+    where emp_no = 101010
+    );
+-- must be a shorter way to do this since just one table?
+
+
+# Find all the titles held by all employees with the first name Aamod.
+# (314 total titles, 6 unique titles)
+
+
+
+# Find all the current department managers that are female.
+
+
+
+
+
+
+# BONUS
+# Find all the department names that currently have female managers.
+
+# Find the first and last name of the employee with the highest salary.
