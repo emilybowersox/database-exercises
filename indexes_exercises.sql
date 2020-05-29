123b36c10f4169c543f00777ab267831213303e6
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-29 11:10:24 -0500

    indexes exercise

---
+++ indexes_exercises.sql
@@ -0,0 +1,20 @@
+use employees;
+
+show tables;
+
+describe employees;
+-- emp_no is the primary key
+describe departments;
+-- pks: dept_no and dept_name
+describe dept_emp;
+-- pks: dept_no and emp_no
+describe dept_manager;
+-- pks: dept_no and emp_no
+describe salaries;
+-- primary keys: emp_no and from_date
+describe titles;
+-- primary keys: emp_no and from_date and title
+
+# use codeup_test_db;
+# describe albums;
+# -- id is the primary key
